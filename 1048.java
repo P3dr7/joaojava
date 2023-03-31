bcd40db0abb49e84d730172b789977f0ca72b806
P3dr7 <100977505+P3dr7@users.noreply.github.com>
2023-03-31 14:36:14 -0300

    Update 1048.java

--- 1048.java
+++ 1048.java
@@ -4,27 +4,28 @@ public class 1048 {
 
     public static void main(String[] args) {
         float salin;
-        double salfin = 0, perc = 0, rea = 0;
+        double salfin = 0, rea = 0;
+        int perc = 0;
         Scanner ler = new Scanner(System.in);
         salin = ler.nextFloat();
         if (salin < 400.0) {
             perc = 15;
             rea = salin * 0.15;
             salfin = salin + rea;
-        } else if (salin > 400 && salin < 800) {
+        } else if (salin > 400 && salin <= 800) {
             perc = 12;
             rea = salin * 0.12;
             salfin = salin + rea;
 
-        } else if (salin > 800 && salin < 1200) {
+        } else if (salin > 800 && salin <= 1200) {
             perc = 10;
             rea = salin * 0.10;
             salfin = salin + rea;
-        } else if (salin > 1200 && salin < 1800) {
+        } else if (salin > 1200 && salin <= 2000) {
             perc = 7;
             rea = salin * 0.07;
             salfin = salin + rea;
-        } else if (salin > 1800) {
+        } else if (salin > 2000) {
             perc = 4;
             rea = salin * 0.04;
             salfin = salin + rea;
